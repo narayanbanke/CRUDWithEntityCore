846b71398e05bc3562107b7390458833ee8d586c
narayanbanke <60840004+narayanbanke@users.noreply.github.com>
2021-01-16 16:01:40 +0530

    Add project files.

---
+++ CRUDEF/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUDEF.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDEF.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EmployeeContext employeeContext ;
+        public HomeController(EmployeeContext sc)
+        {
+            employeeContext = sc;
+        }
+
+        public IActionResult Index()
+        {
+            return View(employeeContext.Employee);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public IActionResult Create_Post(Employee Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee.AddedOn = DateTime.Now;
+                employeeContext.Employee.Add(Employee);
+                employeeContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View(employeeContext.Employee.Where(a => a.Id == id).FirstOrDefault());
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public IActionResult Update_Post(Employee Employee)
+        {
+            employeeContext.Employee.Update(Employee);
+            employeeContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var Employee = employeeContext.Employee.Where(a => a.Id == id).FirstOrDefault();
+            employeeContext.Employee.Remove(Employee);
+            employeeContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUDEF/Models/EmployeeContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CRUDEF.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options) { }
+
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Skills)
+                    .IsRequired()
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary)
+                    .IsRequired()
+                    .HasColumnType("money");
+
+                entity.Property(e => e.AddedOn)
+                    .HasColumnType("date")
+                    .HasDefaultValueSql("(getdate())");
+            });
+        }
+    }
+}
